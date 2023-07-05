0144f787e525756a9ac950aae486d2831ad42480
GiddyK <koechgideon08@gmail.com>
2023-07-05 12:43:02 -0700

    Test driven

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-"""
-Module to add two integers
-"""
+"""Module containing a dummy adder function for testing"""
 
 
 def add_integer(a, b=98):
-    """
-    Adds two numbers which are float or int
-    Casts float to integers then gives the solution
+    """ adds integers
+        Arguments:
+        @a: first integer
+        @b: second integer, defaults to 98 if not given
     """
 
-    if type(a) != int and type(a) != float:
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    elif type(b) != int and type(b) != float:
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    if type(a) is float:
-        a = int(a)
-    if type(b) is float:
-        b = int(b)
-    return a + b
+    return int(a) + int(b)
